3bc993a906055967986ab362c6d5ebf825073b81
kulgg <75735874+kulgg@users.noreply.github.com>
2022-12-04 13:37:05 +0100

    Add day 4

---
+++ advent-of-code/2022/4/a.ts
@@ -0,0 +1,18 @@
+console.log("hi");
+const text = await Deno.readTextFile("input.txt");
+
+const pairs = text.split("\n");
+
+let total = 0;
+pairs.forEach((pair) => {
+  const [[x1, x2], [y1, y2]] = pair
+    .split(",")
+    .map((x) => x.split("-").map((y) => parseInt(y, 10)));
+  if (x1 >= y1 && x2 <= y2) {
+    total += 1;
+  } else if (y1 >= x1 && y2 <= x2) {
+    total += 1;
+  }
+});
+
+console.log("Result", total);
