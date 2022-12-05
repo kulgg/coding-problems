09774755cb24228ec5b68d2914182611c7ec4562
kulgg <75735874+kulgg@users.noreply.github.com>
2022-12-06 00:13:48 +0100

    Add aoc day 5

---
+++ advent-of-code/2022/5/b.ts
@@ -0,0 +1,40 @@
+console.log("Hi");
+
+const text = await Deno.readTextFile("input.txt");
+
+const [stacks_input, instructions] = text.split("\n\n");
+const lines = stacks_input.split("\n");
+const stacks_num = Math.floor((lines[0].length + 1) / 4);
+const stacks: string[][] = [];
+
+for (let i = 0; i < stacks_num; i++) {
+  stacks.push([]);
+}
+
+console.log("stacks", stacks);
+
+for (let j = lines.length - 2; j >= 0; j--) {
+  for (let i = 0; i < stacks_num; i++) {
+    const index = i * 4 + 1;
+    if (lines[j][index] !== " ") {
+      stacks[i].push(lines[j][index]);
+    }
+  }
+}
+
+console.log("stacks", stacks);
+
+const instructions_lines = instructions.split("\n");
+
+for (const instr of instructions_lines) {
+  const segments = instr.split(" ");
+  const amount = parseInt(segments[1]);
+  const fromIndex = parseInt(segments[3]) - 1;
+  const toIndex = parseInt(segments[5]) - 1;
+  console.log(amount, fromIndex, toIndex);
+
+  stacks[toIndex] = stacks[toIndex].concat(stacks[fromIndex].splice(-amount));
+  console.log(stacks);
+}
+
+console.log(stacks.map((x) => x.pop()).join(""));
