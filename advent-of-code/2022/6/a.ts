cd67b99e9bb1d703e776c124eda4d87f9a63211b
kulgg <75735874+kulgg@users.noreply.github.com>
2022-12-07 20:31:55 +0100

    Add day 6

---
+++ advent-of-code/2022/6/a.ts
@@ -0,0 +1,20 @@
+const text = await Deno.readTextFile("input.txt");
+console.log("text", text);
+
+const four: string[] = [];
+let result = 0;
+
+for (let i = 0; i < text.length; i++) {
+  four.push(text[i]);
+  const set = new Set(four);
+  console.log(set);
+  if (set.size === 4) {
+    result = i + 1;
+    break;
+  }
+  if (i >= 3) {
+    four.shift();
+  }
+}
+
+console.log("result", result);
